d446ca19eaacc0902de0d4500996ba7bfabfc2f4
CristiSima <Cristiansimache@gmail.com>
2021-01-08 18:49:13 +0200

    acu face fisieru de test in output

--- check.sh
+++ check.sh
@@ -5,10 +5,11 @@ echo "Checker started"
 #echo ""
 rm output/*/* &> /dev/null
 tests="no_img_loaded_err_P1 no_img_loaded_err_P2 LOAD"
-
+special_tests="SAVE CROP+SELECT ROTATE"
 for test in $tests
 do
 	# echo "Runing test $test"
+	mkdir output/$test
 	cat tests/$test/test | ./image_editor > output/$test/stdout
 	(diff output/$test/stdout tests/$test/stdout >/dev/null && echo "	$test:	OK") || echo "$test: FAILED"
 	# cat tests/$test | ./image_editor
@@ -16,7 +17,8 @@ do
 	# rm out
 done
 
-for test in SAVE CROP+SELECT ROTATE
+for test in $special_tests
 do
+	mkdir output/$test
 	tests/$test/check.sh
 done
